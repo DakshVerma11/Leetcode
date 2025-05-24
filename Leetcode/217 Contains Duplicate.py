e0c3160497f9db2900a2f3e46e9d0d5bbbdb3904
Daksh Verma <66217501+DakshVerma11@users.noreply.github.com>
2025-05-24 12:18:12 +0530

    Create 217 Contains Duplicate.py

---
+++ Leetcode/217 Contains Duplicate.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def containsDuplicate(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+        numset = set(nums)
+        if len(numset)==len(nums):
+            return False
+        return True
