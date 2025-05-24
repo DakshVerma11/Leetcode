69343631ac7e2ef228eecff8d7342617e52d1694
Daksh Verma <66217501+DakshVerma11@users.noreply.github.com>
2025-05-24 13:22:22 +0530

    Create 14 Longest Common Prefix.py

---
+++ Leetcode/14 Longest Common Prefix.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def longestCommonPrefix(self, strs):
+        """
+        :type strs: List[str]
+        :rtype: str
+        """
+        if strs==[]:
+            return ""
+    
+        strs.sort()
+        length = min(len(strs[0]), len(strs[-1]))
+        j=0
+        for i in range(length):
+            if strs[0][i]!=strs[-1][i]:
+                break
+            j+=1
+
+        return strs[0][0:j]
