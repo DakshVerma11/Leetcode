bd68bdcfcc547108e72cc53f76525ad0f990d07a
Daksh Verma <66217501+DakshVerma11@users.noreply.github.com>
2025-05-24 12:56:09 +0530

    Update 242 Valid Anagram.py

--- Leetcode/242 Valid Anagram.py
+++ Leetcode/242 Valid Anagram.py
@@ -14,3 +14,12 @@ class Solution:
                 return False
     
         return True
+
+
+
+
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        sorted_s = sorted(s)
+        sorted_t = sorted(t)
+        return sorted_s == sorted_t
