02917853e48e01c44d5e36ad4a71e8210d40f130
Daksh Verma <66217501+DakshVerma11@users.noreply.github.com>
2025-05-25 23:57:36 +0530

    Update 705 Design HashSet.py

--- Leetcode/705 Design HashSet.py
+++ Leetcode/705 Design HashSet.py
@@ -1,28 +1,30 @@
 class MyHashSet(object):
 
     def __init__(self):
-        
+        self.arr=[False]*1000001
 
     def add(self, key):
         """
         :type key: int
         :rtype: None
         """
+        self.arr[key]=True
+        return
         
-
     def remove(self, key):
         """
         :type key: int
         :rtype: None
         """
-        
+        self.arr[key]=False
+        return 
 
     def contains(self, key):
         """
         :type key: int
         :rtype: bool
         """
-        
+        return self.arr[key]
 
 
 # Your MyHashSet object will be instantiated and called as such:
