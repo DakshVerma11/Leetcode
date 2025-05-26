92191be3f50fbf4ea4c70c59448cbede6ac04031
Daksh Verma <66217501+DakshVerma11@users.noreply.github.com>
2025-05-26 17:17:32 +0530

    Create 304 Range Sum Query 2D - Immutable.py

---
+++ Leetcode/304 Range Sum Query 2D - Immutable.py
@@ -0,0 +1,56 @@
+class NumMatrix(object):
+
+    def __init__(self, matrix):
+        """
+        :type matrix: List[List[int]]
+        """
+        self.matrix = matrix
+
+
+    def sumRegion(self, row1, col1, row2, col2):
+        """
+        :type row1: int
+        :type col1: int
+        :type row2: int
+        :type col2: int
+        :rtype: int
+        """
+        sum=0
+        for i in range(row1,row2+1):
+            for j in range(col1,col2+1):
+                sum+=self.matrix[i][j]
+        return sum
+
+
+
+
+
+class NumMatrix(object):
+
+    def __init__(self, matrix):
+        """
+        :type matrix: List[List[int]]
+        """
+        
+        m, n = len(matrix), len(matrix[0])
+        self.sumrowmatrix = [[0] * (n + 1) for _ in range(m)]
+
+        for i in range(m):
+            for j in range(n):
+                self.sumrowmatrix[i][j + 1] = self.sumrowmatrix[i][j] + matrix[i][j]
+
+
+
+
+    def sumRegion(self, row1, col1, row2, col2):
+        """
+        :type row1: int
+        :type col1: int
+        :type row2: int
+        :type col2: int
+        :rtype: int
+        """
+        total = 0
+        for i in range(row1, row2 + 1):
+            total += self.sumrowmatrix[i][col2 + 1] - self.sumrowmatrix[i][col1]
+        return total
