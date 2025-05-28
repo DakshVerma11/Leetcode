987029f34e540e0afe0b11304ea5e23c26e81cd5
Daksh Verma <66217501+DakshVerma11@users.noreply.github.com>
2025-05-28 13:48:50 +0530

    Create 125. Valid Palindrome.py

---
+++ Leetcode/125. Valid Palindrome.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def isPalindrome(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        s = re.sub(r'[^a-zA-Z0-9]', '', s).lower()
+        for i in range(len(s)//2):
+            if s[i]!=s[-1-i]:
+                return False
+        return True
