87095b3587537ba4916dd74d88fefe78d806b586
Daksh Verma <66217501+DakshVerma11@users.noreply.github.com>
2025-05-28 15:39:19 +0530

    Update 88. Merge Sorted Array.py

--- Leetcode/88. Merge Sorted Array.py
+++ Leetcode/88. Merge Sorted Array.py
@@ -28,4 +28,34 @@ class Solution(object):
             n-=1
             k-=1
         return
+
+
+
+
+class Solution(object):
+    def merge(self, nums1, m, nums2, n):
+        """
+        :type nums1: List[int]
+        :type m: int
+        :type nums2: List[int]
+        :type n: int
+        :rtype: None Do not return anything, modify nums1 in-place instead.
+        """
+        k = m + n - 1
+        m = m - 1
+        n = n - 1
         
+
+        while m >= 0 and n >= 0:
+            if nums1[m] > nums2[n]:
+                nums1[k] = nums1[m]
+                m -= 1
+            else:
+                nums1[k] = nums2[n]
+                n -= 1
+            k -= 1
+
+        while n >= 0:
+            nums1[k] = nums2[n]
+            k -= 1
+            n -= 1
