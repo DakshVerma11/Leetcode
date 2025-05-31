ef333133a24ba12936a8ad4a7b7a1421ef6c0548
Daksh Verma <66217501+DakshVerma11@users.noreply.github.com>
2025-05-31 12:10:28 +0530

    Create 881. Boats to Save People.py

---
+++ Leetcode/881. Boats to Save People.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def numRescueBoats(self, people, limit):
+        """
+        :type people: List[int]
+        :type limit: int
+        :rtype: int
+        """
+        people.sort()
+
+        i=0
+        j=len(people)-1
+        count=0
+        while i<=j:
+            if people[i]+people[j]<=limit:
+                i+=1
+            count+=1
+            j-=1
+            
+
+        return count
