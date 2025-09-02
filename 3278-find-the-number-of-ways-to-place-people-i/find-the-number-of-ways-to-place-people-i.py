9037e56b66418059a4b350a2e99d383bfca90d27
Daksh Verma <66217501+DakshVerma11@users.noreply.github.com>
2025-09-02 21:52:23 +0530

    Time: 10 ms (94.87%) | Memory: 17.7 MB (87.18%) - LeetSync

--- 3278-find-the-number-of-ways-to-place-people-i/find-the-number-of-ways-to-place-people-i.py
+++ 3278-find-the-number-of-ways-to-place-people-i/find-the-number-of-ways-to-place-people-i.py
@@ -1,24 +1,15 @@
 class Solution:
     def numberOfPairs(self, points: List[List[int]]) -> int:
+        points.sort(key=lambda x: (x[0], -x[1]))
         res = 0
-        n = len(points)
-        #points.sort()
-        print(points)
-        for i in range(n):
-            for j in range(n):
-                if i==j:
-                    continue
-                x1, y1 = points[i]
-                x2, y2 = points[j]
 
-                if x1 <= x2 and y1 >= y2:
-                    canbesq = True
-                    #print(x1,y2,'\t',x2,y2)
-                    for k in range(n):
-                        if k != i and k != j and x1 <= points[k][0] <= x2 and y2 <= points[k][1] <= y1:
-                            canbesq = False
-                            break
-                    
-                    if canbesq:
-                        res += 1
+        for i, (x1, y1) in enumerate(points):
+            bottom = -float("inf")
+            for x2, y2 in points[i+1:]:
+                if bottom < y2 <= y1:
+                    res += 1
+                    bottom = y2
+                    if y1 == bottom:
+                        break
+
         return res
