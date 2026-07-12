e5f5399eaa2446afcb708c9a12e258919696e64f
Daksh Verma <66217501+DakshVerma11@users.noreply.github.com>
2026-07-12 14:52:31 +0530

    Time: 0 ms (100.00%) | Memory: 22 MB (77.24%) - LeetSync

--- 450-delete-node-in-a-bst/delete-node-in-a-bst.py
+++ 450-delete-node-in-a-bst/delete-node-in-a-bst.py
@@ -7,52 +7,22 @@
 class Solution:
     def deleteNode(self, root: Optional[TreeNode], key: int) -> Optional[TreeNode]:
         if not root:
-            return root
-
-        parent = None
-        cur = root
-
-        # Find the node to delete
-        while cur and cur.val != key:
-            parent = cur
-            if key > cur.val:
-                cur = cur.right
-            else:
-                cur = cur.left
-
-        if not cur:
-            return root
-
-        # Node with only one child or no child
-        if not cur.left or not cur.right:
-            child = cur.left if cur.left else cur.right
-            if not parent:
-                return child
-            if parent.left == cur:
-                parent.left = child
-            else:
-                parent.right = child
+            return None
+        
+        if root.val>key:
+            root.left=self.deleteNode(root.left,key)
+        elif root.val<key:
+            root.right=self.deleteNode(root.right,key)
         else:
-            # Node with two children
-            par = None  # parent of right subTree min node
-            delNode = cur
-            cur = cur.right
+            if not root.left: 
+                return root.right
+            if not root.right:
+                return  root.left
+
+            cur=root.right
             while cur.left:
-                par = cur
-                cur = cur.left
-
-            if par:  # if there was a left traversal
-                par.left = cur.right
-                cur.right = delNode.right
-
-            cur.left = delNode.left
-
-            if not parent:  # if we're deleting root
-                return cur
-
-            if parent.left == delNode:
-                parent.left = cur
-            else:
-                parent.right = cur
-
+                cur=cur.left
+            root.val=cur.val
+            root.right=self.deleteNode(root.right,cur.val)
         return root
+
