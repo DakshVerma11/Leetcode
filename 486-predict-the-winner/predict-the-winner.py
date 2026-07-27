6efc881a32470e3538edd276c8268463a8ec55a8
Daksh Verma <66217501+DakshVerma11@users.noreply.github.com>
2026-07-27 10:24:19 +0530

    Time: 3 ms (55.97%) | Memory: 19.4 MB (54.73%) - LeetSync

--- 486-predict-the-winner/predict-the-winner.py
+++ 486-predict-the-winner/predict-the-winner.py
@@ -1,17 +1,20 @@
 class Solution:
     def predictTheWinner(self, nums: List[int]) -> bool:
-        memo = {}
+        n = len(nums)
+        # dp[i][j] the max relative score diff for subarray nums[i...j]
+        dp = [[0] * n for _ in range(n)]
         
-        def backtracking(i, j):
-            if i == j:
-                return nums[i]
-            if (i, j) in memo:
-                return memo[(i, j)]
+        for i in range(n):
+            dp[i][i] = nums[i]
             
-            pick_left = nums[i] - backtracking(i + 1, j)
-            pick_right = nums[j] - backtracking(i, j - 1)
-            
-            memo[(i, j)] = max(pick_left, pick_right)
-            return memo[(i, j)]
-            
-        return backtracking(0, len(nums) - 1) >= 0
+        for length in range(2, n + 1):
+            for i in range(n - length + 1):
+                j = i + length - 1
+                
+                # Maximize current pick minus the opponent's subsequent score
+                pick_left = nums[i] - dp[i + 1][j]
+                pick_right = nums[j] - dp[i][j - 1]
+                
+                dp[i][j] = max(pick_left, pick_right)
+                
+        return dp[0][n - 1] >= 0
