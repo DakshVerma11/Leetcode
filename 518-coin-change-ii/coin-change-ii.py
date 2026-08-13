8e4ef1da00b5d31190536d80fe74f6c384c97ac8
Daksh Verma <66217501+DakshVerma11@users.noreply.github.com>
2026-08-14 00:00:18 +0530

    Time: 508 ms (39.59%) | Memory: 52.6 MB (44.75%) - LeetSync

--- 518-coin-change-ii/coin-change-ii.py
+++ 518-coin-change-ii/coin-change-ii.py
@@ -1,18 +1,17 @@
 class Solution:
     def change(self, amount: int, coins: List[int]) -> int:
         coins.sort()
-        n=len(coins)
-        dp=[[-1]*(amount+1) for i in range(n)]
-        def dfs(idx: int, remainingAmount: int) -> int:
-            if remainingAmount == 0:
-                dp[idx][remainingAmount]=1
-                return dp[idx][remainingAmount]
-            if idx >= n or remainingAmount < coins[idx]:
-                return 0
-            if dp[idx][remainingAmount]!=-1:
-                return dp[idx][remainingAmount]
-            dp[idx][remainingAmount] = dfs(idx + 1, remainingAmount)
-            dp[idx][remainingAmount] += dfs(idx, remainingAmount - coins[idx])
-            return dp[idx][remainingAmount]
+        n = len(coins)
+        # dp[i][a] = ways using coins[i:] to make amount a
+        dp = [[0] * (amount + 1) for _ in range(n + 1)]
+        dp[n][0] = 1   # with no coins, only amount 0 is possible
 
-        return dfs(0, amount)
+        for i in range(n - 1, -1, -1):
+            for a in range(amount + 1):
+                # skip current coin
+                dp[i][a] = dp[i + 1][a]
+                # take current coin (if we can)
+                if a >= coins[i]:
+                    dp[i][a] += dp[i][a - coins[i]]
+
+        return dp[0][amount]
